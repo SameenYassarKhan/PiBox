3781491fae14e18ed1b05bb2309556d4a867e457
redtutel <xicoblanco@gmail.com>
2021-03-24 20:16:40 -0700

    Update Icons.py

--- Icons.py
+++ Icons.py
@@ -10,26 +10,32 @@ import os
 #PIL is used to allow the use of .pngs. It needs to installed with typeing pip install Pillow in the terminal
 root = Tk()
 root.title("Main Menu")
-#root.iconbitmap("Iconleak-Atrous-Calculator.ico")
 
 #Defining the button #This still doesn't work
 def openCalculator():
  	os.system('python CalculatorV2.py')
  	
-
+#Add the ability to open up the drawing app too
+def openDrawing():
+	os.system('python DrawingPython.py')
 
 frame = LabelFrame(root, padx = 5, pady = 5)
 frame.pack(padx=10,pady=10)
 
 #This line finds an image
 Calc_img = ImageTk.PhotoImage(Image.open("Iconleak-Atrous-Calculator.ico"))
-#Calc_img = Calc_img.resize((50,50), Image.ANTIALIAS)
-#self.pw.pic = ImageTk.PhotoImage(image)
+
+Draw_img = ImageTk.PhotoImage(Image.open("drawing.ico"))
 
 
 	
 IconC = Button(frame, text = "Calculator", image = Calc_img, compound="top", command = openCalculator)
 
+IconD = Button(frame, text = 'Drawing', image = Draw_img, compound = "top", command = openDrawing)
+
+
+
 IconC.pack()
+IconD.pack()
 
 root.mainloop()

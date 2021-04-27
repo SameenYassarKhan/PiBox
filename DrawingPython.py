ca9bf1fb0485cfdb24bd7f669517c1a2f7df0e70
redtutel <xicoblanco@gmail.com>
2021-04-26 20:06:47 -0700

    Update DrawingPython.py

--- DrawingPython.py
+++ DrawingPython.py
@@ -1,27 +1,77 @@
-import kivy
+from tkinter import *
+from tkinter import ttk, colorchooser
 
-from kivy.app import App
 
-kivy.require('1.9.0')
+class main:
+    def __init__(self,master):
+        self.master = master
+        self.color_fg = 'black'
+        self.color_bg = 'white'
+        self.old_x = None
+        self.old_y = None
+        self.penwidth = 5
+        self.drawWidgets()
+        self.c.bind('<B1-Motion>',self.paint)#drwaing the line 
+        self.c.bind('<ButtonRelease-1>',self.reset)
 
-from kivy.uix.widget import Widget
+    def paint(self,e):
+        if self.old_x and self.old_y:
+            self.c.create_line(self.old_x,self.old_y,e.x,e.y,width=self.penwidth,fill=self.color_fg,capstyle=ROUND,smooth=True)
 
-from kivy.uix.relativelayout import RelativeLayout
+        self.old_x = e.x
+        self.old_y = e.y
 
-class Paint_brush(Widget):
-	pass
+    def reset(self,e):    #reseting or cleaning the canvas 
+        self.old_x = None
+        self.old_y = None      
 
-class Drawing(RelativeLayout):
-	def on_touch_down(self, touch):
-		pb = Paint_brush()
-		pb.center = touch.pos
-		self.add_widget(pb)
-	def on_touch_move(self, touch):
-		pd = Paint_brush()
-		pb.center = touch.pos
-		self.add_widget(pb)
-class DrawingApp(App):
-	def build(self):
-		return(Drawing)
+    def changeW(self,e): #change Width of pen through slider
+        self.penwidth = e
+           
 
-DrawingApp().run()
+    def clear(self):
+        self.c.delete(ALL)
+    
+    def save(self):
+    	self.c.postscript(file = "Drawing" + '.eps')
+    	img = Image.open("Drawing" + '.eps')
+    	img.save("Drawing" + '.png', 'png')
+
+    def change_fg(self):  #changing the pen color
+        self.color_fg=colorchooser.askcolor(color=self.color_fg)[1]
+
+    def change_bg(self):  #changing the background color canvas
+        self.color_bg=colorchooser.askcolor(color=self.color_bg)[1]
+        self.c['bg'] = self.color_bg
+
+    def drawWidgets(self):
+        self.controls = Frame(self.master,padx = 5,pady = 5)
+        Label(self.controls, text='Pen Width:',font=('arial 18')).grid(row=0,column=0)
+        self.slider = ttk.Scale(self.controls,from_= 5, to = 100,command=self.changeW,orient=HORIZONTAL)
+        self.slider.set(self.penwidth)
+        self.slider.grid(row=0,column=1,ipadx=30)
+        self.controls.pack(side=LEFT)
+        
+        self.c = Canvas(self.master,width=500,height=500,bg=self.color_bg,)
+        self.c.pack(fill=BOTH,expand=True)
+
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+        filemenu = Menu(menu)
+        colormenu = Menu(menu)
+        menu.add_cascade(label='Colors',menu=colormenu)
+        colormenu.add_command(label='Brush Color',command=self.change_fg)
+        colormenu.add_command(label='Background Color',command=self.change_bg)
+        optionmenu = Menu(menu)
+        menu.add_cascade(label='Options',menu=optionmenu)
+        optionmenu.add_command(label='Clear Canvas',command=self.clear)
+        optionmenu.add_command(label = 'Save', command=self.save)
+        optionmenu.add_command(label='Exit',command=self.master.destroy) 
+        
+        
+
+if __name__ == '__main__':
+    root = Tk()
+    main(root)
+    root.title('Paint App')
+    root.mainloop()
